0806765fb0ee55ad362a55c5e1872eff5f2c89bb
宏志 朱 <3359005824@qq.com>
2025-03-17 10:53:22 +0800

    重新生成UploadItem表

---
+++ FileService.Infrastructure/Migrations/20250317025221_modify_IsDelete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class modify_IsDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeleteTime",
+                table: "T_UploadItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "T_UploadItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleteTime",
+                table: "T_UploadItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "T_UploadItems");
+        }
+    }
+}
